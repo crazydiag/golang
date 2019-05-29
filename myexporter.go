3a7c00b84f023dbd62b331319a615280f4bc496c
Славин Михаил <slavin@corp.mail.ru>
2019-05-29 18:31:45 +0300

    Second edition

--- myexporter.go
+++ myexporter.go
@@ -24,45 +24,45 @@ var (
    )
 )
 
+func init() {
+	// Metrics have to be registered to be exposed:
+	prometheus.MustRegister(gauge)
+}
+
+
 func main() {
 
   http.Handle("/metrics", promhttp.Handler())
+  r, _ := regexp.Compile("pts")
+  rr, _ := regexp.Compile("tty")
 
-  prometheus.MustRegister(gauge)
+
 
   go func() {
      for {
-        tty := 0 
-        pts := 0 
+         gauge.With(prometheus.Labels{"terminal":"pts"}).Set(float64(0))
+         gauge.With(prometheus.Labels{"terminal":"tty"}).Set(float64(0))
         out, err := exec.Command("who").Output()
         if err != nil {
                 log.Fatal(err)
         }
-        r, _ := regexp.Compile("pts")
-        rr, _ := regexp.Compile("tty")
-//      fmt.Println(r.MatchString(string(out)))
-//      fmt.Printf("%s", out)
         s := strings.Split(string(out), "\n")
         c := regexp.MustCompile("[^\\s]+")
         for i := 0; i < len(s); i++ {
             d := c.FindAllString(s[i], -1)
+//             fmt.Println(d)
             if len(d) > 0 {
             if r.MatchString(string(d[1])) {
-            pts++
+            gauge.With(prometheus.Labels{"terminal":"pts"}).Inc()
             }
             if rr.MatchString(string(d[1])) {
-            tty++
+            gauge.With(prometheus.Labels{"terminal":"tty"}).Inc()
             }
 
-//          fmt.Printf("%d           %s\n", i, s[i])
-//            fmt.Printf("%s ------ %s\n", d[0], d[1])
           }
         }
-        gauge.With(prometheus.Labels{"terminal":"pts"}).Set(float64(pts))
-        gauge.With(prometheus.Labels{"terminal":"tty"}).Set(float64(tty))
-        time.Sleep(100 * time.Second)
-//        time.Sleep(1000 * time.Millisecond)
-//        fmt.Printf("PTS --- %d ; TTY --- %d\n", pts , tty)
+            time.Sleep(10 * time.Second)
+//          time.Sleep(100 * time.Millisecond)
   }
   }()
 
